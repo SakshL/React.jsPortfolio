923f82eb63b317f5461fc55e34ea330f591bf5db
Sakshyam Baral <sakshyam.79137@lfes.edu.np>
2025-08-10 20:34:22 +0545

    Update

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+ extends: ["@igorkowalczyk/eslint-config", "next"],
+ rules: {
+  "react/no-unescaped-entities": "off",
+  "react/prop-types": "off",
+  "@next/next/no-html-link-for-pages": "off",
+  "import/no-anonymous-default-export": "off",
+ },
+ env: {
+  es2022: true,
+  node: true,
+  browser: true,
+ },
+ parserOptions: {
+  ecmaVersion: 2022,
+  sourceType: "module",
+ },
+};
